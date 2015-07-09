0dcf44d9687d2da492e619772b46f3e34dbf9692
Mistri <jmistri7@gmail.com>
2015-07-09 16:07:47 -0700

    Update Blob.java

--- Blob.java
+++ Blob.java
@@ -1,27 +1,41 @@
 package Agar;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 import apcs.Window;
 
 public class Blob {
 	int x;
 	int y;
-
+	
 	int r;
 	int g;
 	int b;
-
+	
 	int radius;
-
+	
 	public Blob() {
 		x = Window.rollDice(10000);
 		y = Window.rollDice(10000);
-		radius = Window.rollDice(20) + 5;
-
+		radius = 5;
+		
 		r = Window.rollDice(255);
 		g = Window.rollDice(255);
 		b = Window.rollDice(255);
+		
+		setValues();
+		Game.ballNumber++;
 	}
-
+	
+	public void setValues() {
+		
+		Game.server.child("ballx"+Game.ballNumber).setValue(x);
+		Game.server.child("bally"+Game.ballNumber).setValue(y);
+		Game.server.child("ballr"+Game.ballNumber).setValue(radius);
+	}
+	
 	public void draw(int xoffset, int yoffset) {
 		Window.out.color(r, g, b);
 		Window.out.circle(Window.width() / 2 + (x - xoffset), Window.height() / 2 + (y - yoffset), radius);
