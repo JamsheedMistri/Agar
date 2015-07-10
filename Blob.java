3bcc81de9c1abf5c4a25ce0aae4d5a7b643c7ee1
Mistri <jmistri7@gmail.com>
2015-07-10 13:25:51 -0700

    Update Blob.java

--- Blob.java
+++ Blob.java
@@ -9,35 +9,42 @@ import apcs.Window;
 public class Blob {
 	int x;
 	int y;
-	
+
 	int r;
 	int g;
 	int b;
-	
+
 	int radius;
-	
+
 	public Blob() {
 		x = Window.rollDice(10000);
 		y = Window.rollDice(10000);
 		radius = 5;
-		
+
 		r = Window.rollDice(255);
 		g = Window.rollDice(255);
 		b = Window.rollDice(255);
-		
+
 		setValues();
 		Game.ballNumber++;
 	}
-	
+
 	public void setValues() {
-		
-		Game.server.child("ballx"+Game.ballNumber).setValue(x);
-		Game.server.child("bally"+Game.ballNumber).setValue(y);
-		Game.server.child("ballr"+Game.ballNumber).setValue(radius);
+
+		Game.server.child("ball").child("ballx"+Game.ballNumber).setValue(x);
+		Game.server.child("ball").child("bally"+Game.ballNumber).setValue(y);
+		Game.server.child("ball").child("ballr"+Game.ballNumber).setValue(radius);
+	}
+
+	public void reset() {
+		x = Window.rollDice(10001) - 1;
+		y = Window.rollDice(10001) - 1;
 	}
 	
 	public void draw(int xoffset, int yoffset) {
-		Window.out.color(r, g, b);
-		Window.out.circle(Window.width() / 2 + (x - xoffset), Window.height() / 2 + (y - yoffset), radius);
+		if (Math.abs(x - xoffset) <= Window.width() / 2 && Math.abs(y - yoffset) <= Window.height() / 2) {
+			Window.out.color(r, g, b);
+			Window.out.circle(Window.width() / 2 + (x - xoffset), Window.height() / 2 + (y - yoffset), radius);
+		}
 	}
 }
